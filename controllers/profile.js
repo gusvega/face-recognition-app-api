93bb04f78d1059b9999ee011c08195dc1fe1ae44
gux <gux.vega@gmail.com>
2020-04-16 10:58:03 -0600

    first commit

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,6 +11,19 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileUpdate = (req, res, db) => {
+  const {id} = req.params
+  const {name, age, pet} = req.body.formInput
+  db('users').where({id}).update({name}).then(resp => {
+    if(resp){
+      res.json('SUCCESS')
+    }else{
+      res.status(400).json('Unable to update')
+    }
+  }).catch(err => res.status(400).json('Error Updating'))
+}
+
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 }
--- server.js
+++ server.js
@@ -8,28 +8,28 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const morgan = require('morgan')
+const auth = require('./controllers/authorization')
+
+console.log(process.env.POSTGRES_HOST, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, process.env.POSTGRES_DB)
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
-
 app.use(cors())
 app.use(bodyParser.json());
+app.use(morgan('combined'))
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.get('/', (req, res)=> { res.send('IT IS WORKING') })
+app.post('/signin', signin.signInAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => {profile.handleProfileUpdate(req, res, db)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
